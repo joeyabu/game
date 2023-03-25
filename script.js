9446b2c7b24635b5046278a28998a306e738e2f8
joeyabu <124846445+joeyabu@users.noreply.github.com>
2023-03-25 10:28:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ const message = document.querySelector('.message');
 let truckInterval;
 
 function getRandomSpeed() {
-  const speeds = [5, 8, 12];
+  const speeds = [5, 8, 10];
   return speeds[Math.floor(Math.random() * speeds.length)];
 }
 
 function resetGame() {
-  clearInterval(truckInterval);
-  truck.style.right = '0';
-  truck.style.bottom = 'calc(33.33%)';
-  tatsuro.style.bottom = '50px';
-  tatsuro.style.transform = 'rotate(0deg)';
-  message.style.display = 'none';
-  startBtn.disabled = false;
-}
+    clearInterval(truckInterval);
+    truck.style.right = '0';
+    truck.style.top = 'calc(400px)'; // 修正
+    tatsuro.style.left = 'calc(15px)'; // 修正
+    tatsuro.style.bottom = '100px'; // 修正
+    tatsuro.style.transform = 'translate(-50%, 0)'; // 修正
+    message.style.display = 'none';
+    startBtn.disabled = false;
+  }
 
 startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
@@ -32,7 +33,7 @@ startBtn.addEventListener('click', () => {
 
 jumpBtn.addEventListener('click', () => {
   clearInterval(truckInterval);
-  tatsuro.style.bottom = '25%';
+  tatsuro.style.bottom = '30%';
   tatsuro.style.transform = 'translateY(-50%)';
 
   setTimeout(() => {
@@ -44,16 +45,18 @@ jumpBtn.addEventListener('click', () => {
     message.style.display = 'block';
     if (distance < 0) {
       tatsuro.style.transform = 'translateY(-50%) rotate(-90deg)';
-      message.style.left = `${truckRect.left}px`; // road.pngに被らないように位置を修正
-      message.style.top = `${tatsuroRect.top - 50}px`; // road.pngに被らないように位置を修正
+      message.style.left = `${tatsuroRect.right}px`; // road.pngに被らないように位置を修正
+      message.style.top = `${tatsuroRect.top - 30}px`; // road.pngに被らないように位置を修正
       message.textContent = '死○じゃった';
       message.classList.add('failure');
     } else if (distance <= 35) {
       message.textContent = '僕は死にませーん';
       message.classList.add('success');
+      message.style.left = `${tatsuroRect.left + tatsuroRect.width / 2}px`; // 位置を修正
+      message.style.top = `${tatsuroRect.top - message.offsetHeight}px`; // 位置を修正
     } else {
-      message.style.left = `${tatsuroRect.right}px`; // road.pngに被らないように位置を修正
-      message.style.top = `${tatsuroRect.top - 50}px`; // road.pngに被らないように位置を修正
+      message.style.left = `${tatsuroRect.left + tatsuroRect.width / 2}px`; // 位置を修正
+      message.style.top = `${tatsuroRect.top - 15}px`; // 位置を修正
       message.textContent = '根性なし';
       message.classList.add('no-guts');
     }
